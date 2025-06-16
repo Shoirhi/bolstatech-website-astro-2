f9e4c67c8af7384ecec95e4a7e09aa1f18dadaec
Shoirhi <violins-medal5r@icloud.com>
2025-06-16 22:20:14 +0900

    feat: add Google Analytics integration using Partytown for improved performance

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,10 +7,16 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://bolstatech.com",
-  integrations: [sitemap(), react()],
+  integrations: [sitemap(), react(), partytown({
+    config: {
+      forward: ["dataLayer.push"],
+    },
+  })],
 
   vite: {
     plugins: [tailwindcss()]
